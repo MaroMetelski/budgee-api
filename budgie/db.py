89d240ec84966a4cecbe93f556ea576bfcaeb0d9
Marek Metelski <marek.metelski01@gmail.com>
2022-12-12 23:17:19 +0100

    Improve error handling
    Improve slightly backend and API error handling:
    Handle exceptions and return None / empty objects when data
    can't be found in database. Return True / False to indicate database
    "post" status.
    Handle backend errors and return sensible HTTP response codes in the
    API.

--- budgie/db.py
+++ budgie/db.py
@@ -9,6 +9,7 @@ from sqlalchemy import (
     UniqueConstraint,
 )
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import uuid
@@ -106,8 +107,12 @@ class Database:
                 type=account["type"],
                 extra_type=account["extra_type"],
             )
-            session.add(acc)
-            session.commit()
+            try:
+                session.add(acc)
+                session.commit()
+                return True
+            except IntegrityError:
+                return False
 
     def create_tag(self, tag: str):
         with sessionmaker(self.db).begin() as session:
@@ -115,8 +120,12 @@ class Database:
                 user_id=self.current_uid,
                 tag=tag,
             )
-            session.add(tag)
-            session.commit()
+            try:
+                session.add(tag)
+                session.commit()
+                return True
+            except IntegrityError:
+                return False
 
     def create_user(self, user):
         with sessionmaker(self.db).begin() as session:
@@ -127,8 +136,12 @@ class Database:
                 salt=user["salt"],
                 created=user["created"],
             )
-            session.add(user_m)
-            session.commit()
+            try:
+                session.add(user_m)
+                session.commit()
+                return True
+            except IntegrityError:
+                return False
 
     def get_user(self, email):
         with sessionmaker(self.db).begin() as session:
@@ -167,6 +180,9 @@ class Database:
                 .filter_by(name=entry["credit_account"])
                 .first()
             )
+            if not credit_account or not debit_account:
+                return False
+
             entry_m.credit_account = credit_account
             entry_m.debit_account = debit_account
 
@@ -182,6 +198,7 @@ class Database:
                     entry_m.tags.append(entry_tag)
 
             session.add(entry_m)
+            return True
 
     def delete_entry(self, entry_id):
         with sessionmaker(self.db).begin() as session:
